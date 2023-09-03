727719187d5ba5996c3314c84b6e52283ebf6bae
baa <34665120+baa-mb@users.noreply.github.com>
2023-09-03 16:14:00 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
-    basic.showIcon(IconNames.SmallHeart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     basic.pause(2000)
+    basic.clearScreen()
     basic.showString("SOLAR")
     serial.writeValue("x", input.lightLevel())
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
