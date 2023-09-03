c2ce6084e4c55564b3e79a0c9158e25ec6249be0
baa <34665120+baa-mb@users.noreply.github.com>
2023-09-03 16:17:50 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,8 @@ basic.showIcon(IconNames.Yes)
 let zzz = 0
 basic.forever(function () {
     zzz += 1
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(2000)
     basic.showNumber(zzz)
+    basic.pause(2000)
     serial.writeValue("x", input.lightLevel())
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 })
